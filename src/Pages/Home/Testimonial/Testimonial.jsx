a709870ed33372788855a1008e3b73dfbdc36f03
Joy <github.joysaha@gmail.com>
2024-05-16 22:58:53 +0600

    testimonial page design changes

--- src/Pages/Home/Testimonial/Testimonial.jsx
+++ src/Pages/Home/Testimonial/Testimonial.jsx
@@ -1,27 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
 import { Navigation } from "swiper/modules";
+import { Rating } from "@smastrom/react-rating";
+import "@smastrom/react-rating/style.css";
 
 const Testimonial = () => {
+  const [reviews, setReviews] = useState([]);
+
+  useEffect(() => {
+    fetch("reviews.json")
+      .then((res) => res.json())
+      .then((data) => setReviews(data));
+  }, []);
+
   return (
-    <section className="my-20">
+    <section className="my-20 max-w-6xl mx-auto">
       <SectionTitle
         subHeading={"What Our Client say"}
         heading={"Testimonial"}
       ></SectionTitle>
       <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
+        {reviews.map((review) => (
+          <SwiperSlide key={review._id}>
+            <div className="flex flex-col items-center mx-24 my-16">
+              <Rating
+                style={{ maxWidth: 180 }}
+                value={review.rating}
+                readOnly
+              />
+              <p>{review.details}</p>
+              <h3 className="text-2xl text-orange-400">{review.name}</h3>
+            </div>
+          </SwiperSlide>
+        ))}
       </Swiper>
     </section>
   );
