ae37f0540e451cb1d739d89ffb53d3ad30c82a1b
Joy <github.joysaha@gmail.com>
2024-05-16 20:27:19 +0600

    Popular item data fetching and show data

--- src/Pages/Home/PopularMenu/PopularMenu.jsx
+++ src/Pages/Home/PopularMenu/PopularMenu.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import SectionTitle from '../../../Components/SectionTitle/SectionTitle';
+import MenuItems from '../../../Components/MenuItems/MenuItems';
 
 const PopularMenu = () => {
     const [menuItems, setMenuItems] = useState([])
@@ -14,8 +15,13 @@ const PopularMenu = () => {
     }, [])
 
     return (
-        <section>
+        <section className='max-w-6xl mx-auto mb-12'>
             <SectionTitle heading={"From Our Menu"} subHeading={"Popular Items"} ></SectionTitle>
+            <div className='grid md:grid-cols-2 gap-4'>
+                {
+                    menuItems.map((item) => <MenuItems key={item._id} item={item}></MenuItems>)
+                }
+            </div>
         </section>
     );
 };
