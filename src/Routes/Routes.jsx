779eac9269ceb1e998aa7601183f8595fe80c64a
Joy <github.joysaha@gmail.com>
2024-05-16 17:21:14 +0600

    first commit

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,18 @@
+import Main from "../Layouts/Main";
+import { createBrowserRouter } from "react-router-dom";
+import Home from "../Pages/Home/Home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+        {
+            path: "/",
+            element:<Home></Home>
+        }
+    ]
+  },
+]);
+
+export default router;
