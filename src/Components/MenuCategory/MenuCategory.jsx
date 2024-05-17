a285e3135d897f665d64e88129c0cbe186d623a0
Joy <github.joysaha@gmail.com>
2024-05-17 09:58:53 +0600

    order food section completed

--- src/Components/MenuCategory/MenuCategory.jsx
+++ src/Components/MenuCategory/MenuCategory.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import MenuItems from '../MenuItems/MenuItems';
+import Coverd from '../Coverd/Coverd';
 
-const MenuCategory = () => {
+const MenuCategory = ({items, title, coverImg}) => {
     return (
-        <div>
-            
+        <div className=' pt-8'>
+            { title && <Coverd title={title} img={coverImg}></Coverd>  }
+             <div className='grid md:grid-cols-2 gap-4 max-w-6xl mx-auto my-16'>
+                {
+                    items.map((item) => <MenuItems key={item._id} item={item}></MenuItems>)
+                }
+            </div>
         </div>
     );
 };
--- src/Pages/Home/Menu/Menu.jsx
+++ src/Pages/Home/Menu/Menu.jsx
@@ -2,16 +2,31 @@ import React from 'react';
 import Coverd from '../../../Components/Coverd/Coverd';
 import PopularMenu from '../PopularMenu/PopularMenu';
 import useMenu from '../../../Hooks/useMenu';
+import SectionTitle from '../../../Components/SectionTitle/SectionTitle';
+import MenuCategory from '../../../Components/MenuCategory/MenuCategory';
 
 const Menu = () => {
     const [menu] = useMenu();
 
+    const salad = menu.filter(item => item.category === 'salad');
+    const drinks = menu.filter(item => item.category === 'drinks');
+    const popular = menu.filter(item => item.category === 'popular');
+    const dessert = menu.filter(item => item.category === 'dessert');
+    const pizza = menu.filter(item => item.category === 'pizza');
+    const soup = menu.filter(item => item.category === 'soup');
+    const offered = menu.filter(item => item.category === 'offered');
+
     return (
         <div>
             <Coverd title={"Our Menu"} img={"https://i.ibb.co/5BzXC2T/banner3.jpg"}></Coverd>
-            <PopularMenu></PopularMenu>
-           
-           
+            {/* main category */}
+            <SectionTitle subHeading={"Don't Miss"} heading={"Today's Offer"}></SectionTitle>
+            {/* offer */}
+            <MenuCategory items={offered}></MenuCategory>
+            {/* desert */}
+            <MenuCategory items={dessert} title={"Desert"} coverImg={"https://i.ibb.co/MV1pFVB/dessert-bg.jpg"}></MenuCategory>
+            <MenuCategory items={pizza} title={"Pizza"} coverImg={"https://i.postimg.cc/NFmkQy3w/pia-bg.jpg"}></MenuCategory>
+            <MenuCategory items={salad} title={"Salad"} coverImg={"https://i.postimg.cc/8k6HR3jn/salad-bg.jpg"}></MenuCategory>
         </div>
     );
 };
